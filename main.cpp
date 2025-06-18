ec9050305b35e2e4ac27bdd0db5f8a43712983bc
Sarah Compres <sarahcompres@Sarahs-MacBook-Air.local>
2025-06-18 13:06:22 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostrea>
+
+int main () {
+    const int SIZE = 5 
+    int number[SIZE];
+
+    std::count << "Enter " << SIZE << "number:";
+
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << "number:" << (i + 1) << ": ";
+        std::cin << numbers[i];
+    }
+
+
