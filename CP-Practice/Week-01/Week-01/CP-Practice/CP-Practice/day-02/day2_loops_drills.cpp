a2d5b29d4c0d0edea0809a5380f14cc225d1f552
Sarah Compres <sarahcompres@Sarahs-MacBook-Air.local>
2025-06-22 15:55:55 -0400

    ✅ Day 2: Loops, strings, 3 CF problems, logic challenges

---
+++ CP-Practice/Week-01/Week-01/CP-Practice/CP-Practice/day-02/day2_loops_drills.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0)
+    cout << "FizzBuzz" << endl;
+    else if (i % 3 == 0)
+    cout << "Fizz" << endl;
+    else if (i % 5 == 0)
+    cout << "Buzz" << endl;
+    }
+    int n;
+    cout << "enter a number" << endl;
+    cin >> n;
+    
+    int count = 0;
+
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) 
+        sum += 1;
+    }
+    cout << "sum of odd number from 1 to" << n << ": " << sum << endl;
+
+    return
+}
